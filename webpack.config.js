32eac2f1df39d8870e904c20ebe347f4f25c7566
肖鹏 <xiaopeng1@sf-express.com>
2017-03-15 18:46:40 +0800

    multi entry

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,33 @@
  var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports={
-    context: '.',
     entry:{
-        "main":'./src/script/main.js',
-        "a":'./src/script/a.js'
+        'main':'./src/script/main.js',
+        'a':'./src/script/a.js',
+        'b':'./src/script/b.js',
+        'c':'./src/script/c.js'
     },
     output:{
         path:'./dist',
-        filename:'js/[name]-[chunkhash].js'
+        filename:'js/[name]-[chunkhash].js',
+        publicPath:'http://www.sharp.com'
     },
     plugins: [
         new htmlWebpackPlugin({
-            filename: './index.html',
+            filename: 'a.html',
             template: 'index.html',
-            inject:'head',
-            title:'this is a title',
-            date:new Date()
+            title:'this is a title',    //一个title 属性
+            inject:'body',   //注入head/body/ture/false true为body false为不注入
+            date:new Date(),
+            chunks:['main','a']
+        }),
+        new htmlWebpackPlugin({
+            filename: 'b.html',
+            template: 'index.html',
+            title:'this is b title',    //一个title 属性
+            inject:'body',   //注入head/body/ture/false true为body false为不注入
+            date:new Date(),
+            chunks:['b','c']
         })
     ]
 
